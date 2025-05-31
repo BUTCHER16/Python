33791f5d3c1ce8053b1f829303e518d39df44274
BUTCHER16 <ravidossalbert@gmail.com>
2025-05-31 21:22:06 +0530

    Need TO Complete

---
+++ Learn Python/task_4.py
@@ -0,0 +1,12 @@
+class task4:
+
+    def lora(self):
+
+        for i in range(1, 101):
+            sum = []
+
+            if i % 2 == 0:
+                sum.append()    
+
+            return "Even" if i%2==0 else "Odd"
+        
