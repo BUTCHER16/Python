4735a24003088c6b588a17ba122d220cca518f1b
BUTCHER16 <ravidossalbert@gmail.com>
2025-06-02 21:51:38 +0530

    Commit

---
+++ Learn Python/task_8.py
@@ -0,0 +1,13 @@
+def Lamda_fun():
+
+    add_ten = lambda x: x + 10
+
+    multiply = lambda x,y : x * y
+
+    return True if multiply % 2 == 0 else False
+
+
+print(Lamda_fun.add_ten(5))
+
+print(Lamda_fun.multiply(4,7))
+
